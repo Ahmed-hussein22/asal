c977dd866956784419c7df6ecb0fc490f4486d00
Ahmed-hussein22 <ahmedhusseien01141028987@gmail.com>
2026-04-27 14:46:34 +0300

    update prodeuct dreess

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,11 @@ import { Footer } from './component/footer/footer';
 import { Product } from './component/product/product';
 import { Slider } from './component/slider/slider';
 import { About } from './component/about/about';
+import { Productdresses } from './component/productdresses/productdresses';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Navbar,Footer,Product,Slider,About],
+  imports: [RouterOutlet,Navbar,Footer,Product,Slider,About,Productdresses],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
